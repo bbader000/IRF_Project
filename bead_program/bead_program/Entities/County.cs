6055af5c80793b745636be6041f78abba5b30d6e
Bence Zsolt Báder <bence.bader@uni-corvinus.hu>
2020-12-11 20:42:56 +0100

    kezdés

---
+++ bead_program/bead_program/Entities/County.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bead_program.Entities
+{
+    public class County
+    {
+
+        public string name { get; set; }
+        public int area { get; set; }
+        public double forest { get; set; }
+        public int rain { get; set; }
+
+        public County()
+        {
+            
+        }
+
+        
+    }
+}
--- bead_program/bead_program/Form1.cs
+++ bead_program/bead_program/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using bead_program.Entities;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -7,14 +8,51 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml;
+using System.Xml.Linq;
 
 namespace bead_program
 {
     public partial class Form1 : Form
     {
+        public List<County> counties = new List<County>();
+
         public Form1()
         {
             InitializeComponent();
+            loadCounties();
+
+            int a = 1;
+
+        }
+
+         void loadCounties()
+        {
+            XDocument xdocument = XDocument.Load("countydata.xml");
+            IEnumerable<XElement> data = xdocument.Root.Elements();
+
+            foreach (var row in data)
+            {
+                counties.Add(new County()
+                {
+                    name = row.Element("County").Value,
+                    area = int.Parse(row.Element("Area").Value),
+                    forest = double.Parse(row.Element("Area").Value),
+                    rain = int.Parse(row.Element("Area").Value)
+                }
+                );
+
+
+
+
+
+
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
